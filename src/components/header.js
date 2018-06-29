ae11f5034b5d42bd645c842b000c1d32f96d742c
alex <akourtisdev@gmail.com>
2018-06-30 01:05:53 +0300

    first commit

---
+++ src/components/header.js
@@ -0,0 +1,16 @@
+import React,{ Component } from 'react';
+import '../css/styles.css'
+
+class Header extends Component{
+    
+    render(){
+        return (
+            <header className='header'>
+                <div className='logo'>logo</div>
+                <input type="text" />
+            </header>
+        )
+    }
+}
+
+export default Header;
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// COMPONENTS
+import Header from './components/header.js';
+
+const App = () => {
+    return (
+        <div>
+            <Header/>
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
