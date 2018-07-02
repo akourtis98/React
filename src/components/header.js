857fdb650ca80d9239f47985bb0445b6755446c3
alex <akourtisdev@gmail.com>
2018-07-02 04:23:31 +0300

    ..

--- src/components/header.js
+++ src/components/header.js
@@ -2,12 +2,22 @@ import React,{ Component } from 'react';
 import '../css/styles.css'
 
 class Header extends Component{
-    
+    state = {
+        keywords: ''
+    }
+
+    inputChangeHandler(e){
+        this.setState({
+            keywords: e.target.value
+        })
+    }
+
     render(){
         return (
             <header className='header'>
                 <div className='logo'>logo</div>
-                <input type="text" />
+                <input type="text" 
+                onChange={this.inputChangeHandler.bind(this)}/>
             </header>
         )
     }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
+import JSON from './db.json';
 // COMPONENTS
 import Header from './components/header.js';
+import NewsList from './components/news_list.js'
 
-const App = () => {
-    return (
-        <div>
-            <Header/>
-        </div>
-    )
+class App extends Component{
+    
+    state = {
+        news: JSON
+    }
+    render(){
+        console.log(this.state.news);
+        return (
+            <div>
+                <Header/>
+                <NewsList news={this.state.news}/>
+            </div>
+        )
+    }
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'));
