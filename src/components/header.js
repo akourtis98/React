9da7f011cd098a4bf5e118e9769ce925e8baabc6
alex <akourtisdev@gmail.com>
2018-07-02 05:23:40 +0300

    ...

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,7 @@ import '../css/styles.css'
 
 const Header = props => {
         return (
-            <header style={style} className='header'>
+            <header className='header'>
                 <div className='logo'>logo</div>
                 <input type="text" 
                 onChange={props.keywords}/>
--- src/index.js
+++ src/index.js
@@ -9,19 +9,29 @@ import NewsList from './components/news_list.js'
 class App extends Component{
     
     state = {
-        news: JSON
+        news: JSON,
+        filtered: []
     }
     
-    getKeyword = e => {
-        console.log(e.target.value)
+    getKeyword = (e) => {
+        let keyword = e.target.value;
+        let filtered = this.state.news.filter((item) => {
+            return item.title.index0f(keyword) > -1
+        })
+
+        this.setState({
+            filtered
+        })
     }
 
     render(){
-        console.log(this.state.news);
+        let newFiltered = this.state.filtered;
+        let newsWhole = this.state.news
         return (
             <div>
                 <Header keywords={this.getKeyword}/>
-                <NewsList news={this.state.news}/>
+                <NewsList news={this.state.filtered.length === 0 ? 
+                    newsWhole : newFiltered}/>
             </div>
         )
     }
