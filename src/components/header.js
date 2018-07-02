119254cab03c3d2773ff191770056ce1b14957a2
alex <akourtisdev@gmail.com>
2018-07-02 05:06:50 +0300

    removed mistake

--- src/components/header.js
+++ src/components/header.js
@@ -2,16 +2,6 @@ import React from 'react';
 import '../css/styles.css'
 
 const Header = props => {
-        const style = {
-            background: 'red'
-        }
-
-        if (props.keywords !== ''){
-            style.background = 'blue'
-        }else{
-            style.background = 'red'
-        }
-
         return (
             <header style={style} className='header'>
                 <div className='logo'>logo</div>
